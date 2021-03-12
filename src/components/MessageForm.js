4eac837f7f0939938023ecd5aa27c3898c68951e
HeeyeonJeong <junghy14971@gmail.com>
2021-03-12 17:48:11 +0900

    Style: contact component styles

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,15 +1,83 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useContext } from "react";
+import styled, { css } from "styled-components";
 import { useInput } from "../hooks/useInput";
 import { send } from "emailjs-com";
+import { ThemeContext } from "../context/ThemeContext";
 
 export const useId = process.env.REACT_APP_USER_ID;
 export const templateId = process.env.REACT_APP_TEMPLATE_ID;
 export const serviceID = process.env.REACT_APP_SERVICE_ID;
 
-const Form = styled.form``;
+const FormBox = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid;
+  padding: ${(props) => props.theme.paddingL};
+  border-radius: 5px;
+
+  label {
+    margin-bottom: ${(props) => props.theme.marginS};
+  }
+
+  input {
+    border: none;
+    height: 40px;
+    padding: ${(props) => props.theme.paddingS};
+    margin-bottom: ${(props) => props.theme.marginL};
+    border-radius: 5px;
+  }
+
+  @media ${(props) => props.theme.tabletSmall} {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  @media ${(props) => props.theme.deskTop} {
+    width: 40%;
+    margin: 0;
+  }
+`;
+
+const MessageInput = styled.textarea`
+  border: none;
+  resize: vertical;
+  padding: ${(props) => props.theme.paddingS};
+  margin-bottom: ${(props) => props.theme.marginL};
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  border: 1px solid;
+  border-radius: 5px;
+  padding: ${(props) => props.theme.paddingS};
+  transition: 100ms all ease-out;
+
+  ${(props) =>
+    props.bgColor === "#181818"
+      ? css`
+          :hover {
+            color: #181818;
+            background-color: white;
+          }
+        `
+      : css`
+          :hover {
+            background-color: black;
+            color: #f1efed;
+          }
+        `}
+
+  @media ${(props) => props.theme.tabletSmall} {
+    width: 50%;
+    margin: 0 auto;
+  }
+`;
 
 function MessageForm() {
+  const colorMode = useContext(ThemeContext);
+
   const [{ name, email, message }, onChange, reset] = useInput({
     name: "",
     email: "",
@@ -31,7 +99,7 @@ function MessageForm() {
   };
 
   return (
-    <Form onSubmit={onSubmit}>
+    <FormBox onSubmit={onSubmit}>
       <label htmlFor="name">Name</label>
       <input
         type="text"
@@ -51,17 +119,19 @@ function MessageForm() {
         placeholder="메일 주소를 입력해주세요."
       />
       <label htmlFor="message">Message</label>
-      <textarea
+      <MessageInput
         name="message"
-        rows="5"
+        rows="10"
         value={message}
         onChange={onChange}
         required
         placeholder="내용을 입력해주세요."
-      ></textarea>
-      <button type="submit">메일 보내기</button>
-    </Form>
+      ></MessageInput>
+      <Button type="submit" bgColor={colorMode.mode.bgColor}>
+        메일 보내기
+      </Button>
+    </FormBox>
   );
 }
 
-export default MessageForm;
+export default React.memo(MessageForm);
--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -1,25 +1,110 @@
 import React from "react";
 import styled from "styled-components";
+import * as FaIcons from "react-icons/fa";
+import * as FiIcons from "react-icons/fi";
 import MessageForm from "../MessageForm";
 
 const Section = styled.section`
   min-height: 100vh;
+  /* width: 100%; */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
   margin: ${(props) => props.theme.marginL} 0;
-  padding: ${(props) => props.theme.paddingL} 0;
+  padding: ${(props) => props.theme.paddingL};
 `;
 
-const InfoBox = styled.ul``;
+const Title = styled.h2`
+  text-align: center;
+  font-size: ${(props) => props.theme.fontXL};
+  margin-bottom: ${(props) => props.theme.marginL};
+
+  @media ${(props) => props.theme.tabletSmall} {
+    margin-bottom: ${(props) => props.theme.marginXXL};
+  }
+`;
+
+const ContactItem = styled.div`
+  width: 100%;
+
+  @media ${(props) => props.theme.deskTop} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const InfoBox = styled.ul`
+  text-align: center;
+  margin-bottom: ${(props) => props.theme.marginL};
+
+  @media ${(props) => props.theme.deskTop} {
+    width: 40%;
+  }
+`;
+
+const Info = styled.li`
+  margin-bottom: ${(props) => props.theme.marginM};
+
+  p {
+    font-weight: 700;
+    margin-bottom: ${(props) => props.theme.marginS};
+  }
+
+  a {
+    font-family: "Noto Sans KR", sans-serif;
+    :hover {
+      text-decoration: underline;
+    }
+  }
+
+  @media ${(props) => props.theme.tabletSmall} {
+    margin-bottom: ${(props) => props.theme.marginXXL};
+    font-size: ${(props) => props.theme.fontM};
+  }
+`;
 
 function Contact() {
   return (
     <Section id="contact">
-      <InfoBox></InfoBox>
-      <MessageForm />
+      <Title>Contact</Title>
+      <ContactItem>
+        <InfoBox>
+          <Info>
+            <p>
+              <FiIcons.FiMail /> Mail
+            </p>
+            <a href="mailto:junghy14971@gmail.com">junghy14971@gmail.com</a>
+          </Info>
+          <Info>
+            <p>
+              <FaIcons.FaGithubSquare /> GitHub
+            </p>
+            <a
+              href="https://github.com/HeeyeonJeong"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              https://github.com/HeeyeonJeong
+            </a>
+          </Info>
+          <Info>
+            <p>
+              <FaIcons.FaBlogger /> Blog
+            </p>
+            <a
+              href="https://heeyeonjeong.tistory.com"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              https://heeyeonjeong.tistory.com
+            </a>
+          </Info>
+        </InfoBox>
+        <MessageForm />
+      </ContactItem>
     </Section>
   );
 }

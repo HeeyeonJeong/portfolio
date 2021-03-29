601827856937264f149c07a9989305f2ad722e04
HeeyeonJeong <junghy14971@gmail.com>
2021-03-30 01:36:58 +0900

    Update: resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ function Header() {
       </Title>
       {isTabletDown || (
         <ResumeLink
-          href="https://drive.google.com/file/d/19mGRhPEJUZrwGGzIHwcZ2ApX8guJ7jcr/view?usp=sharing"
+          href="https://drive.google.com/file/d/10EVc71c_G5gDX0wR0Uez8bbbCCbMEg4e/view?usp=sharing"
           target="_blank"
           rel="noreferrer noopener"
         >
